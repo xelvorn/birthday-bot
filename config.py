4d76ec33674c1fe6dffcd31b58cf3debf2a4bfca
xelrn <imxelvorn@gmail.com>
2025-05-04 19:00:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ BOT_TOKEN = "7011629657:AAFZ7rX1dGqJa_UZWFSQ_8D_MORPtaZ31zU"
 CHANNEL_ID = -1002551914932  # ID твоего канала или чата
 BIRTHDAY_MONTH = 5  # месяц дня рождения
 BIRTHDAY_DAY = 8    # день месяца
+
+# данные являются примерными, не пытайтесь спиздить токен)
